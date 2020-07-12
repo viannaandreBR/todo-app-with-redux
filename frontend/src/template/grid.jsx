68ca6bd1826dcb715d5321118305e6d8c5987ea2
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-11 22:07:33 -0300

    Commit Frontend Grid & iconButton

---
+++ frontend/src/template/grid.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+
+export default class Grid extends Component{
+ 
+  toCssClasses(numbers){
+    const cols = numbers ? numbers.split(' ') : []
+    let classes = ''
+
+    if (cols[0]) classes += ` col-xs-${cols[0] }`
+    if (cols[1]) classes += ` col-xs-${cols[1] }`
+    if (cols[2]) classes += ` col-xs-${cols[2] }`
+    if (cols[3]) classes += ` col-xs-${cols[3] }`
+    
+    return classes
+  }
+
+  render() {
+    const gridClasses = this.toCssClasses(this.props.cols || 12) 
+    return (
+      <div className={gridClasses}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
-  <div>
+  <div role='form' className='todoForm' >
 
-    <h1>Form</h1>
+    <Grid cols='12 9 10'>
+   
+      <input id='description' className='form-control'
+        placeholder='Adicione uma tarefa'></input>
 
+    </Grid>
+
+
+    <Grid cols='12 3 2'>
+
+      <IconButton style='primary' icon='plus'>
+
+      </IconButton>
+
+    </Grid>
+    
   </div>
 )
