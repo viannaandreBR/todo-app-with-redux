332830c3800ca900f9d504b7c173b4027c9e2972
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-15 05:48:35 -0300

    Apply Middleware with Redux-Promise

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-  description: 'Ler Livro',
-  list: [{
-    _id: 1,
-    description: 'Pagar fatura do cartão',
-    done: true
-  },{
-    _id: 2,
-    description: 'Reunião com a equipe às 10:00',
-    done: false
-  },{
-    _id: 3,
-    description: 'Consulta Médica na terça depois do Almoço',
-    done: false
-  }]
-}
+const INITIAL_STATE = { description: '' , list: [] }
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type){
