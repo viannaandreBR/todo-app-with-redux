b3afdebd9814df5fb3d024fc845a9711b8e2493f
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-15 04:02:58 -0300

    Deploy Redux Dev Tools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './main/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import { createStore } from 'redux'
+import App from "./main/app";
+import reducers from "./main/reducers";
 
-import { Provider } from 'react-redux'
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-import reducers from './main/reducers'
-
-const store = createStore(reducers)
+const store = createStore(reducers, devTools);
 
 ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-<Provider store={store}>
-
-  <App/>
-
-</Provider>
-
-, document.getElementById('app'))
+  document.getElementById("app")
+);
